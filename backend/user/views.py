7fd1980f9e0f4857766c3ce00b803c5906d09bd9
Alex <adolf5341@gmail.com>
2023-11-30 07:28:26 +0200

    create new endpoint get_user

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.utils.translation import gettext as _
+from django.http import Http404
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+
+from .serializers import UserRetrieve
+from .models import User
+
+
+@api_view(['GET'])
+def get_user(request, pk: int):
+    user: User = User.objects.filter(pk=pk).last()
+    if not user:
+        raise NotFound(detail=_('User with pk: %(pk)s not found') % {'pk': pk}, code=1)
+    serializer = UserRetrieve(user)
+    serialized_data = serializer.data
+    return Response(serialized_data, status.HTTP_200_OK)
