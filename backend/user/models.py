261114fd59397145c60a5443641c0484b54df6a5
Alex <adolf5341@gmail.com>
2023-11-30 05:44:07 +0200

    create user model with email authentication

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
@@ -5,7 +6,36 @@ from django.utils.translation import gettext as _
 
 
 class UserManager(BaseUserManager):
-    pass
+
+    use_in_migrations = True
+    
+    def _create_user(self, email: str, password: str, **extra_fields) -> AbstractUser:
+        if not email:
+            raise ValueError(_('Users require an email field'))
+        email = self.normalize_email(email)
+        user: AbstractUser = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+
+    def create_user(self, email: str, password: Optional[str] = None, **extra_fields) -> AbstractUser:
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+
+    def create_superuser(self, email: str, password: str, **extra_fields) -> AbstractUser:
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -14,6 +44,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meata:
         verbose_name = _('User')
